4a82e39099198d7d06fcbc00939efe8f84d4b77e
karuta0825 <magune3g@gmail.com>
2020-12-13 10:30:39 +0900

    update

--- src/operation/core.cljs
+++ src/operation/core.cljs
@@ -1,63 +1,118 @@
 (ns operation.core
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require ["googleapis" :refer [google]])
-  (:require ["fs" :as fs])
-  (:require ["readline" :as readline])
-  (:require [cljs.core.async :refer [<! chan >! put! take! timeout] :as async]))
+  (:require
+    [cljs.core.async :as async :refer [<! >! chan put!]]
+    ["fs" :as fs]
+    ["googleapis" :refer [google]]
+    ["readline" :as readline])
+  (:require-macros
+    [cljs.core.async.macros :refer [go]]))
 
 ;; jsオブジェクトのmethodがないかもしれないという警告を黙らせる
 (set! *warn-on-infer* false)
 
+
 (def SCOPES ["https://www.googleapis.com/auth/spreadsheets"])
 
 (def TOKEN_PATH "token.json")
+(def CREDENTIAL_PATH "credentials.json")
 
-(def SHEET {:spreadsheetId "1XRKKdU4rTlK4Tiz-HPHp8_CAgBdbskh54s0_QoJv1Xs"
-            :range "tasks!A1:E13"})
 
-(defn js->clj-key [js-obj]
+(def SHEET
+  {:spreadsheetId "1chKCceg5UejYfEEZ7KSGIlYcb9-EjHWF0dbEMuEaie4"
+   :range "request!A3:E"})
+
+
+(defn ss-range
+  [sheet range])
+
+
+(def alphabet
+  (map #((comp clojure.string/upper-case char) %) (range 97 (+ 97 26))))
+
+
+(defn twenty-six-base
+  [n]
+  (loop [num n
+         result ()]
+    (if (not= num 0)
+      (recur (quot num 26) (conj result (dec (mod num 26))))
+      result)))
+
+
+(defn get-column
+  [num]
+  (clojure.string/join (map #(nth alphabet %) (twenty-six-base num))))
+
+
+(nth alphabet 25)
+
+
+(map first (filter (fn [[idx num]] (even? num)) (map-indexed vector (range 10 20))))
+
+
+(defn get-range
+  ([start-column start-row end-column]
+   (let [sc (get-column start-column)
+         ec (get-column end-column)]
+     (str sc start-row ":" ec)))
+  ([start-column start-row end-column end-row]
+   (let [sc (get-column start-column)
+         ec (get-column end-column)]
+     (str sc start-row ":" ec end-row))))
+
+
+(get-range 1 10 10)
+
+
+(defn js->clj-key
+  [js-obj]
   (js->clj js-obj :keywordize-keys true))
 
-(defn read-file [file out-chan]
+
+(defn read-file
+  [file out-chan]
   (.readFile fs
              file
              (fn [err content]
                (if (nil? err)
                  (go (>! out-chan content))
-                 (put! out-chan "{\"error\": true}"))))
-  out-chan)
+                 (put! out-chan "{\"error\": true}")))))
 
-(defn write-file [file-name content]
+
+(defn write-file
+  [file-name content]
   (.writeFile fs file-name content, (fn [err]
                                       (if (nil? err)
                                         (println (str "Token stored to " TOKEN_PATH))
                                         (.error js/console err)))))
 
-;; (defn authorize [oAuthClient token-chan]
-;;   (let [ch (chan)]
-;;     (println "i---------------------")
-;;     (println "in authorize")
-;;     (println "i---------------------")
-;;     (go (let [token (<! token-chan)]
-;;           (println "t---------------------")
-;;           (println token)
-;;           (println "t---------------------")
-;;           (.setCredentials oAuthClient token)
-;;           (>! ch oAuthClient)))
-;;     ch))
 
-(defn authorize [oAuthClient token]
-  (.setCredentials oAuthClient token)
-  oAuthClient)
+(defn authorize
+  [authenticated-client token-chan]
+  (let [ch (chan)]
+    (go (let [token (<! token-chan)]
+          (.setCredentials authenticated-client token)
+          (>! ch authenticated-client)))
+    ch))
 
 
-(defn mk-auth-client [credential]
+(defn mk-auth-client
+  [credential]
   (let [{:keys [client_secret client_id redirect_uris]} (:installed credential)
-        oAuthClient (new (.-OAuth2 (.-auth google)) client_id client_secret (nth redirect_uris 0))]
-    oAuthClient))
+        authenticated-client (new (.-OAuth2 (.-auth google))
+                                  client_id
+                                  client_secret
+                                  (nth redirect_uris 0))]
+    authenticated-client))
+;
+
+(defn generate-url
+  [authClient]
+  (.generateAuthUrl authClient (clj->js {:access_type "offline" :scope SCOPES})))
 
 
-(defn read-cmdline [authClient]
+(defn read-cmdline
+  [authClient]
   (let [ch (chan)
         auth-url (.generateAuthUrl authClient (clj->js {:access_type "offline" :scope SCOPES}))
         rl (.createInterface readline (clj->js {:input (.-stdin js/process)
@@ -70,26 +125,35 @@
                  (put! ch code)))
     ch))
 
-(defn get-new-token [authClient cmd]
+
+(defn token-file?
+  [file]
+  "token fileが存在するかどうか"
+  (let [ch (chan)]
+    (.stat fs file (fn [err stat] (put! ch (nil? err))))
+    ch))
+
+
+(defn get-new-token
+  [authClient cmd]
   (let [ch (chan)]
     (.getToken authClient cmd (fn [err token]
                                 (write-file TOKEN_PATH (.stringify js/JSON token))
                                 (put! ch token)))
     ch))
 
-(defn get-token [authClient file]
-  (go (let [content (<! (read-file file (chan 1 (map (.-parse js/JSON)))))]
-        (if (.-error content)
-          (let [cmd (<! (read-cmdline authClient))]
-            ;; こうしないといけないのはなぜか？
-            ;; なるほど処理がすすんでしまうのだ
-            ;; いや型をみればこれでただしいんだよ
-            (<! (get-new-token authClient cmd)))
-          content))))
 
-(defn get-sheet-value [auth params]
+(defn get-token
+  [authClient file]
+  (go (if (<! (token-file? file))
+        (<! (read-file file (chan 1 (map (.-parse js/JSON)))))
+        (<! (get-new-token authClient (<! (read-cmdline authClient)))))))
+
+
+(defn get-sheet-value
+  [auth-chan params]
   (let [ch (chan)]
-    (go (let [
+    (go (let [auth (<! auth-chan)
               args (clj->js {:version "v4" :auth auth})
               sheets (.sheets google args)]
 
@@ -99,91 +163,182 @@
                 (fn [err content]
                   (if (nil? err)
                     (put! ch (js->clj content :keywordize-keys true))
-                    (println err))))
-          ))
-    ch))
-
-(defn update-sheet-value [auth-chan params]
-  (let [ch (chan)]
-    (go (let [args (clj->js {:version "v4" :auth (<! auth-chan)})
-              sheets (.sheets google args)]
-
-          (.update (.-values (.-spreadsheets sheets))
-                (clj->js params)
-                (fn [res err]
-                  (println err)
-                  (println res)))))
+                    (println err))))))
     ch))
 
 
-(defn output-chan [in-ch]
+(rem 10 10)
+
+
+(defn output-chan
+  [in-ch]
   (go (let [res (<! in-ch)
             values (get-in res [:data :values])]
-        (mapv (fn [row] (println row)) values))))
-
-(defn main []
-  (go (let [cred (<! (read-file "credentials.json" (chan 1 (map (comp js->clj-key (.-parse js/JSON))))))
-            authClient (mk-auth-client cred)
-;            token-ch (read-file "token.json" (chan 1 (map (.-parse js/JSON))))
-            ; 明示的に止めたい場合を考慮しないといけない
-            token (<! (get-token authClient "token.json"))
-            auth-chan (authorize authClient token)
-            ]
-
-       (output-chan (get-sheet-value auth-chan SHEET))
-;       (println (<! (read-cmdline)))
-;        (println (<! auth-chan))
-    ))
-)
+        (mapv (fn [row] (prn row)) values))))
 
 
-;; (go (let [token-ch (read-file "a" (chan))]
-;;       (println (<! token-ch))))
-
-;; (let [input-chan (chan)
-;;       our-pub (async/pub input-chan :msg-type)
-;;       output-chan (chan)]
-
-;;   (async/sub our-pub :greeting output-chan)
-
-;;   (go-loop []
-;;     (let [{:keys [text]} (<! output-chan)]
-;;       (println text)
-;;       (recur)))
-
-;;   (put! input-chan {:msg-type :greeting :text "hi"}))
+(defn main
+  []
+  ;; (go (let [cred (<! (read-file CREDENTIAL_PATH (chan 1 (map (comp js->clj-key (.-parse js/JSON))))))
+  ;;           authClient (mk-auth-client cred)
+  ;;           token-ch (get-token authClient TOKEN_PATH)
+  ;;           auth-chan (authorize authClient token-ch)
+  ;;           ]
+  ;;       (output-chan (get-sheet-value auth-chan SHEET))
+  ;;       ))
+  )
 
 
-;; (let [ch (chan)
-;;       new-ch (chan)
-;;       up-ch (chan)
-;;       de-ch (chan)
-;;       ; どのkeyで分岐するかきめる
-;;       pub (async/pub ch :msg-type)]
+(defn my-read-file
+  [in-ch out-ch ex-ch]
+  (go (let [file (<! in-ch)]
+        (.readFile fs
+                   file
+                   (fn [err content]
+                     (if (nil? err)
+                       (put! out-ch content)
+                       (put! ex-ch err)))))))
 
-;;   ; pub-chanのkeyのどの値のときにどこのチャンネルに送るのかをきめる
-;;   (async/sub pub :new new-ch)
-;;   (async/sub pub :upd up-ch)
-;;   (async/sub pub :del de-ch)
 
-;;   (go (println (<! new-ch)))
-;;   (go (println (<! up-ch)))
-;;   (go (println (<! de-ch)))
+(defn make-auth-client
+  [auth-client-ch token-ch output-ch]
+  (go (let [authenticated-client (<! auth-client-ch)
+            token (<! token-ch)]
+        (.setCredentials authenticated-client token)
+        (>! output-ch authenticated-client))))
 
-;;   ;; (go-loop []
-;;   ;;   (let [n (<! new-ch)]
-;;   ;;     (println (str "n" n))
-;;   ;;     (recur)))
 
-;;   ;; (go-loop []
-;;   ;;   (let [u (<! up-ch)]
-;;   ;;     (println (str "u" u))
-;;   ;;     (recur)))
+(defn make-new-token
+  [auth-ch passcode-ch token-ch]
+  (go (let [authClient (<! auth-ch)
+            passcode (<! passcode-ch)]
+        (.getToken authClient passcode (fn [err token]
+                                         (write-file TOKEN_PATH (.stringify js/JSON token))
+                                         (put! token-ch (.stringify js/JSON token)))))))
 
-;;   ;; (go-loop []
-;;   ;;   (let [u (<! de-ch)]
-;;   ;;     (println (str "u" u))
-;;   ;;     (recur)))
 
-;;   (put! ch {:msg-type :del :text "hiiiiiiiiiiiiiiiiiii"})
-;; )
+(defn ask-passcode
+  [req-token-ch auth-url-ch passcode-ch]
+  (go (let [url (<! auth-url-ch)
+            _ (<! req-token-ch)
+            rl (.createInterface readline (clj->js {:input (.-stdin js/process)
+                                                    :output (.-stdout js/process)}))]
+        (println (str "Authorize this app by visiting this url:" url))
+        (.question rl "Enter the code from that page here: "
+                   (fn [code]
+                     (.close rl)
+                     (put! passcode-ch code))))))
+
+
+(defn gain-sheet-value
+  [auth-ch params output-ch]
+  (go (let [args (clj->js {:version "v4" :auth (<! auth-ch)})
+            sheets (.sheets google args)]
+        (.get (.-values (.-spreadsheets sheets))
+              (clj->js params)
+              (fn [err content]
+                (if (nil? err)
+                  (put! output-ch (js->clj content :keywordize-keys true))
+                  (println err)))))))
+
+
+(defn search-update-row-idx
+  [id sheet-values]
+  (->> sheet-values
+       (map-indexed vector)
+       (filter (fn [[idx row]] (= (nth row 0) (str id))))
+       (map (comp inc first))))
+
+
+(defn update-sheet-value
+  [auth-ch params output-ch]
+  (go (let [args (clj->js {:version "v4" :auth (<! auth-ch)})
+            sheets (.sheets google args)]
+
+        (.update (.-values (.-spreadsheets sheets))
+                 (clj->js params)
+                 (fn [res err]
+                   (println err)
+                   (println res))))))
+
+
+(defn my-string
+  [row]
+  (let [no (nth row 0)
+        date (nth row 1)
+        name (nth row 2)
+        content (nth row 3)]
+    (str
+      "** TODO 依頼" no "
+   :PROPERTIES:
+   :id:    " no "
+   :date:  " date "
+   :name:   " name "
+   :END:
+   
+   [内容]
+   " content "
+   ")))
+
+
+(go (let [credential-path (chan)
+          credential (chan 1 (map (comp mk-auth-client js->clj-key (.-parse js/JSON))))
+          auth-url (chan 1 (map generate-url))
+          req-auth (chan)
+          token-path (chan)
+          req-token (chan)
+          tmp (chan)
+          token (chan 1 (map (.-parse js/JSON)))
+          passcode (chan)
+          authed-client (chan)
+          m-auth (async/mult credential)
+          out-ch (chan)
+          exception (chan)]
+
+      (async/tap m-auth auth-url)
+      (async/tap m-auth req-auth)
+      (async/tap m-auth tmp)
+
+      (my-read-file credential-path credential exception)
+      (my-read-file token-path token req-token)
+      (ask-passcode req-token auth-url passcode)
+      (make-new-token tmp passcode token)
+      (make-auth-client req-auth token authed-client)
+      (gain-sheet-value authed-client SHEET out-ch)
+
+      (go (>! credential-path CREDENTIAL_PATH))
+      (go (>! token-path TOKEN_PATH))
+
+      (->> (get-in (<! out-ch) [:data :values])
+;           (drop 1)
+ ;          (map #(my-string %))
+           (search-update-row-idx 3)
+           (mapv #(println %)))))
+
+
+;; (let [service "ac"
+;;       no 1
+;;       grp "CS"
+;;       name "nene"
+;;       userId 356184
+;;       priority "当日必須（決済）"
+;;       deadline "2020/12/10"
+;;       wfid "3150-26"
+;;       background "test"]
+;;   (println
+;;     (str
+;;       "** TODO " service " " no "
+;;    :PROPERTIES:
+;;    :id:       " no "
+;;    :group:    " grp "
+;;    :name:     " name "
+;;    :userId:   " userId "
+;;    :priority: " priority "
+;;    :deadline: " deadline "
+;;    :wfid:     " wfid "
+;;    :Effort:   00:30
+;;    :END:" "
+
+;;    [背景]" background)))
+
+
